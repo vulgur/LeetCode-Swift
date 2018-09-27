516760dc2b28e3be9406504705057336e35d2225
Wang Shudao <shudao.wang@me.com>
2018-09-27 15:44:27 +0800

    Add solution 412

---
+++ 412_FizzBuzz.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func fizzBuzz(_ n: Int) -> [String] {
+        var result = [String]()
+        for i in 1...n {
+            if i%3==0 && i%5==0 {
+                result.append("FizzBuzz")
+            } else if i%3==0 {
+                result.append("Fizz")
+            } else if i%5==0 {
+                result.append("Buzz")
+            } else {
+                result.append(String(i))
+            }
+        }
+        return result
+    }
+}
